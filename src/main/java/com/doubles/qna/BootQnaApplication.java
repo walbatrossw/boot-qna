0dca62e764a5e3317baf7cff821689a97b8771ef
DESKTOP-TTECIDV\walba <walbatrossw@gmail.com>
2017-06-19 17:12:11 +0900

    initial commit

---
+++ src/main/java/com/doubles/qna/BootQnaApplication.java
@@ -0,0 +1,12 @@
+package com.doubles.qna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootQnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootQnaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/doubles/qna/BootQnaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.doubles.qna;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootQnaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
