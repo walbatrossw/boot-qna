5eafc5a7e2a8e82dac9515b90d3d5f9d621a6fdd
DESKTOP-TTECIDV\walba <walbatrossw@gmail.com>
2017-06-20 14:37:12 +0900

    8th commit [html파일 및 url 정리]

--- src/main/java/com/doubles/qna/web/UserController.java
+++ src/main/java/com/doubles/qna/web/UserController.java
@@ -7,21 +7,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
+    // 회원가입 화면
+    @GetMapping("/form")
+    public String form() {
+        return "user/form";
+    }
+    // 회원가입 처리
     @PostMapping("/create")
     public String create(User user) {
         System.out.println("User : " + user);
         userRepository.save(user);
-        return "redirect:list";
+        return "redirect:/user/list";
     }
 
+    // 회원 리스트
     @GetMapping("/list")
     public String list(Model model) {
         model.addAttribute("users", userRepository.findAll());
